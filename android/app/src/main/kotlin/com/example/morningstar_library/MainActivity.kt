0c7e003ddb2edd96320e74c1200a932c70e40c0e
Athhar Kautsar <athharkautsar14@gmail.com>
2023-10-29 19:39:20 +0700

    feat(splash): creating splash screen

--- android/app/src/main/kotlin/com/example/morningstar_library/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/morningstar_library/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.morningstar_library
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+  override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
